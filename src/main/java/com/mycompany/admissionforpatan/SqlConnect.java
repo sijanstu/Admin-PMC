bcfe2a24aa557dbc9f2098110050e62c0921159a
Sijan Bhandari <bhandarisijanstu@gmail.com>
2020-06-11 18:34:09 +0545

    doc update

--- src/main/java/com/mycompany/admissionforpatan/SqlConnect.java
+++ src/main/java/com/mycompany/admissionforpatan/SqlConnect.java
@@ -1,26 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.admissionforpatan;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 /**
  *
- * @author Programming-learning
+ * @author Sijan Bhandari
  */
+/*This Class can be used to check database connection in mysql
+    use dependancy in pom file to use mysql connector automatically:
+        <dependency>
+            <groupId>mysql</groupId>
+            <artifactId>mysql-connector-java</artifactId>
+            <version>8.0.18</version>
+        </dependency>
+*/
 public class SqlConnect {
     String response;
     SqlConnect(String server, String port, String database, boolean unicode, boolean ugdbcc, boolean uldc, String zone, String user, String pass)throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");  
         Connection con;//=DriverManager.getConnection("jdbc:mysql://localhost:3306/patan_admin","root","");
         con = DriverManager.getConnection("jdbc:mysql://"+server+":"+port+"/"+database+"?"+"useUnicode="+unicode+"&useJDBCCompliantTimezoneShift="+ugdbcc+"&useLegacyDatetimeCode="+uldc+"&serverTimezone="+zone,user,pass);
-
-             // DriverManager.getConnection("jdbc:mysql://localhost:3306/resultout? useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root",""))
         if (con != null) {
             response="Connected to server";
             con.close();
@@ -35,6 +34,12 @@ public class SqlConnect {
     }
     
 }
+
+
+
+
+
+
 
 
 
