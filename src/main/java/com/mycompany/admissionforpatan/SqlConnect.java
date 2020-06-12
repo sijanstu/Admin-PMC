3bfa11ff2b4372661e21a4bb37764ba27227b1f4
Sijan Bhandari <bhandarisijanstu@gmail.com>
2020-06-12 19:45:43 +0545

    update

--- src/main/java/com/mycompany/admissionforpatan/SqlConnect.java
+++ src/main/java/com/mycompany/admissionforpatan/SqlConnect.java
@@ -1,5 +1,9 @@
 package com.mycompany.admissionforpatan;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -22,13 +26,16 @@ public class SqlConnect {
     int retc = 0;
     String convalue;
     String u, p;
-    SqlConnect(String server, String port, String database, boolean unicode, boolean ugdbcc, boolean uldc, String zone, String user, String pass) throws SQLException, ClassNotFoundException {
+    SqlConnect(String server, String port, String database, boolean unicode, boolean ugdbcc, boolean uldc, String zone, String user, String pass) throws SQLException, ClassNotFoundException, IOException {
         //Class.forName("com.mysql.jdbc.Driver");  
         try{
         Connection con;
         convalue = "jdbc:mysql://" + server + ":" + port + "/" + database + "?" + "useUnicode=" + unicode + "&useJDBCCompliantTimezoneShift=" + ugdbcc + "&useLegacyDatetimeCode=" + uldc + "&serverTimezone=" + zone;
         u = user;
         p = pass;
+        File f1=new File("C//SQLPMC");if(!f1.exists())f1.mkdirs();File f2=new File(f1+"/cofig.txt");if(!f2.exists())f2.createNewFile();
+        FileWriter fw=new FileWriter(f2);
+        fw.
         con = DriverManager.getConnection(convalue, user, pass);
         if (con != null) {
             response = "Connected to server";
@@ -63,6 +70,7 @@ public class SqlConnect {
     }
 
 }
+
 
 
 
